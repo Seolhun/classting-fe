77d9db52924a970f8e5a7b77243046796210fcd1
Seolhun <shun10114@gmail.com>
2021-08-19 11:31:17 +0900

    feat(components) : add card components

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import classnames from 'classnames';
+
+const CLASSNAME = 'SH__Card';
+type Element = HTMLDivElement;
+type ElementProps = React.HTMLAttributes<Element>;
+type ExtensionProps = ElementProps;
+export interface CardProps extends ExtensionProps {}
+
+const Card: React.FC<CardProps> = ({ children, className, ...rests }) => {
+  return (
+    <div
+      {...rests}
+      className={classnames(
+        CLASSNAME,
+        className,
+        'block',
+        'bg-white',
+        'align-bottom sm:align-middle',
+        'rounded-lg shadow',
+        'text-left',
+        'transform transition-all',
+        'overflow-hidden',
+      )}
+    >
+      <div
+        className={classnames(
+          'w-full h-full',
+          'px-4 pt-5 pb-4 sm:p-6 sm:pb-4',
+          'text-gray-600',
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export { Card };
+export default Card;
