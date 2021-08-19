ca0fb083b2ed20b58d8691876a357a49e13727aa
Seolhun <shun10114@gmail.com>
2021-08-19 17:02:27 +0900

    chore(tailwind) : update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,14 @@ module.exports = {
   ],
   theme: {
     screens: {
-      sm: '480px',
-      // => @media (min-width: 480px) { ... }
-      md: '768px',
-      // => @media (min-width: 768px) { ... }
+      xl: '1440px',
+      // => @media (min-width: 1440px) { ... }
       lg: '976px',
       // => @media (min-width: 976px) { ... }
-      xl: '1440px'
-      // => @media (min-width: 1440px) { ... }
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+      sm: '480px',
+      // => @media (min-width: 480px) { ... }
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
