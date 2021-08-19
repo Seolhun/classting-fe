d1f9a3ecf764771315109821799c7e807f43f5b8
Seolhun <shun10114@gmail.com>
2021-08-19 11:40:33 +0900

    feat(containers) : add question containers

--- src/containers/questions/Question.tsx
+++ src/containers/questions/Question.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-const Question = () => {
-  return <div>Question</div>;
+import { WorkbookQuestionModel } from '@/models';
+import { Card, H4, Tag, Checkbox, Radio } from '@/components';
+
+export interface QuestionProps extends WorkbookQuestionModel {}
+
+const Question: React.FC<QuestionProps> = ({
+  category,
+  type,
+  difficulty,
+  question,
+  correct_answer,
+  incorrect_answers,
+}) => {
+  const { t } = useTranslation();
+
+  const memoAllQuestions = React.useMemo(() => {
+    return [...incorrect_answers, correct_answer].sort(
+      () => Math.random() * 10 - Math.random() * 10,
+    );
+  }, [correct_answer, incorrect_answers.length]);
+
+  return (
+    <Card>
+      <div className="">{category}</div>
+      <div>
+        <Tag>{t(`workbooks:difficulties.${difficulty}`)}</Tag>
+        <Tag>{t(`workbooks:type.${type}`)}</Tag>
+      </div>
+      <div className="mt-2">
+        <H4>{question}</H4>
+        {memoAllQuestions.map((memoQuestion, i) => (
+          <div key={i}>
+            {type === 'multiple' ? (
+              <Checkbox name={`${question}-${i}`}>{memoQuestion}</Checkbox>
+            ) : (
+              <Radio name={`${question}-${i}`}>{memoQuestion}</Radio>
+            )}
+          </div>
+        ))}
+      </div>
+    </Card>
+  );
 };
 
+export { Question };
 export default Question;
