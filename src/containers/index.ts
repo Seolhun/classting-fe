09a09c152d1c5e901a3d7ffb77585b624ad7745c
Seolhun <shun10114@gmail.com>
2021-08-19 11:44:17 +0900

    feat(containers) : update root containers files changing export modules directories name

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -2,6 +2,4 @@ export * from './notes';
 export * from './questions';
 export * from './routes';
 export * from './statistics';
-export * from './summaries';
-export * from './summaries';
 export * from './workbooks';
