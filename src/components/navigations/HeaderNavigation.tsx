2c1aae043567be05611339cd5ae9e1f926bacb47
Seolhun <shun10114@gmail.com>
2021-08-19 17:02:14 +0900

    feat(components) : update header navigation screen size

--- src/components/navigations/HeaderNavigation.tsx
+++ src/components/navigations/HeaderNavigation.tsx
@@ -72,7 +72,7 @@ const HeaderNavigation: React.FC<HeaderNavigationProps> = ({
 
   return (
     <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-gray-800">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 bg-gray-800">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">{logo}</div>
@@ -94,31 +94,6 @@ const HeaderNavigation: React.FC<HeaderNavigationProps> = ({
                   </button>
                 ))}
               </div>
-            </div>
-          </div>
-          <div className="hidden md:block">
-            <div className="ml-4 flex items-center md:ml-6">
-              <button
-                type="button"
-                className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              >
-                <span className="sr-only">View notifications</span>
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                  />
-                </svg>
-              </button>
             </div>
           </div>
           <div className="-mr-2 flex md:hidden">
