6a882d4287639b31f326a62b31f493ac3bc1738a
Seolhun <shun10114@gmail.com>
2021-08-19 17:00:51 +0900

    feat(hooks) : remove unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './useIntersectionObserver';
 export * from './useLockScroll';
-export * from './useMediaQuery';
 export * from './usePageState';
 export * from './usePagination';
 export * from './useStep';
