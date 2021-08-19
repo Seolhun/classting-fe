41c23cbe748669ff106a58bc9407b0d133d16d6b
Seolhun <shun10114@gmail.com>
2021-08-19 11:30:56 +0900

    feat(components) : update button component props

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,24 @@ import { I18nextProvider } from 'react-i18next';
 import { RecoilRoot } from 'recoil';
 
 import i18n from './i18n';
-import { ToastProvider } from './components';
+import { ErrorBoundary, ToastProvider } from './components';
 import { MainRoutes } from './pages/MainRoutes';
 
 const Root = () => {
   return (
-    <RecoilRoot>
-      <ToastProvider>
-        <I18nextProvider i18n={i18n}>
-          <HashRouter>
-            <React.Suspense fallback={null}>
-              <MainRoutes />
-            </React.Suspense>
-          </HashRouter>
-        </I18nextProvider>
-      </ToastProvider>
-    </RecoilRoot>
+    <ErrorBoundary>
+      <RecoilRoot>
+        <ToastProvider>
+          <I18nextProvider i18n={i18n}>
+            <HashRouter>
+              <React.Suspense fallback={null}>
+                <MainRoutes />
+              </React.Suspense>
+            </HashRouter>
+          </I18nextProvider>
+        </ToastProvider>
+      </RecoilRoot>
+    </ErrorBoundary>
   );
 };
 
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import classnames from 'classnames';
 
-type ElementProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+import { ColorWeight, IntentType } from '@/types';
+import { createColorByIntent, createOptionsColorByIntent } from '@/utils';
+
+const CLASSNAME = 'SH__Button';
+type Element = HTMLButtonElement;
+type ElementProps = React.ButtonHTMLAttributes<Element>;
 type ExtensionProps = ElementProps;
 
 export interface ButtonProps extends ExtensionProps {
@@ -18,15 +23,16 @@ export interface ButtonProps extends ExtensionProps {
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
-    { className, children, intent = 'primary', intentWeight = 500, ...props },
+    { className, children, intent = 'primary', intentWeight = 500, ...rests },
     ref,
   ) => {
     return (
       <button
         ref={ref}
         type="button"
-        {...props}
+        {...rests}
         className={classnames(
+          CLASSNAME,
           className,
           'inline-flex items-center justify-center',
           'py-2 px-4',
