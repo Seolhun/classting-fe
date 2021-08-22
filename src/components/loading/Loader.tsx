dbbf402c3a87d303b925063641290de1db6de569
Seolhun <shun10114@gmail.com>
2021-08-23 08:58:51 +0900

    feat(componetns) : update loader component props

--- src/components/loading/Loader.tsx
+++ src/components/loading/Loader.tsx
@@ -4,9 +4,9 @@ import classnames from 'classnames';
 const CLASSNAME = 'SH__Loader';
 type ElementProps = React.HTMLAttributes<HTMLDivElement>;
 type ExtensionProps = ElementProps;
-export interface LoaderProps extends ExtensionProps {}
+export type LoaderProps = ExtensionProps;
 
-const Loader: React.FC<LoaderProps> = ({ children, className, ...rests }) => {
+const Loader: React.FC<LoaderProps> = ({ className, ...rests }) => {
   return (
     <div {...rests} className={classnames(CLASSNAME, className)}>
       <div className="ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12"></div>
