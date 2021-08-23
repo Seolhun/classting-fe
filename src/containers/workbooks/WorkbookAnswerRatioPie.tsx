0c7122c9c1ec2c8c1634ec6c77f1a3aca1d50324
Seolhun <shun10114@gmail.com>
2021-08-23 10:43:43 +0900

    feat(chart) : add ratio pie chart

---
+++ src/containers/workbooks/WorkbookAnswerRatioPie.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { AutoSizer } from 'react-virtualized';
+import { Pie } from '@nivo/pie';
+
+interface WorkbookAnswerRatioPieProps {
+  data: any;
+}
+
+const WorkbookAnswerRatioPie: React.FC<WorkbookAnswerRatioPieProps> = ({
+  data,
+}) => (
+  <AutoSizer>
+    {({ height, width }) => (
+      <Pie
+        data={data}
+        height={height}
+        width={width}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        activeOuterRadiusOffset={8}
+        borderWidth={1}
+        borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: 'color' }}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{ from: 'color', modifiers: [['darker', 2]] }}
+        colors={['rgb(37, 99, 235)', 'rgb(234, 88, 12)']}
+        defs={[
+          {
+            id: 'dots',
+            type: 'patternDots',
+            background: 'inherit',
+            color: 'rgba(255, 255, 255, 0.3)',
+            size: 4,
+            padding: 1,
+            stagger: true,
+          },
+          {
+            id: 'lines',
+            type: 'patternLines',
+            background: 'inherit',
+            color: 'rgba(255, 255, 255, 0.3)',
+            rotation: -45,
+            lineWidth: 6,
+            spacing: 10,
+          },
+        ]}
+        legends={[
+          {
+            anchor: 'bottom',
+            direction: 'row',
+            justify: false,
+            translateX: 0,
+            translateY: 56,
+            itemsSpacing: 0,
+            itemWidth: 100,
+            itemHeight: 18,
+            itemTextColor: '#999',
+            itemDirection: 'left-to-right',
+            itemOpacity: 1,
+            symbolSize: 18,
+            symbolShape: 'circle',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemTextColor: '#000',
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    )}
+  </AutoSizer>
+);
+
+export { WorkbookAnswerRatioPie };
+export default WorkbookAnswerRatioPie;
