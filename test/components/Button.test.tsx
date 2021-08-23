9a0f1ff1f12a3f76644ff68d0e75a7b16fb6c1c0
Seolhun <shun10114@gmail.com>
2021-08-23 12:08:54 +0900

    chore(test) : update button test

--- test/components/Button.test.tsx
+++ test/components/Button.test.tsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { render } from "@testing-library/react";
+import Button from '../../src/components/button/Button'
 
-function Button() {
-  return <button className="btn-success">btn-success</button>;
-}
 
-describe("Button Test", () => {
-  const button = render(<Button />);
+describe('Button Test', () => {
+  test('Button', () => {
+    let button = render(<Button>classting</Button>);
+    const { container } = button;
 
-  test("Button contains text", () => {
-    expect(button.getByText("btn-success"));
+    expect(button.getByText('classting'));
+    expect(container.classList.contains('SH__Button'));
+  });
+
+  test('Button: Intent', () => {
+    let button = render(<Button>classting</Button>);
+    expect(button.container.getElementsByClassName('bg-blue-500').length).toBe(1);
+    button = render(<Button intent='success'>classting</Button>);
+    expect(button.container.getElementsByClassName('bg-green-500').length).toBe(1);
+  });
+
+  test('Button: Intent Weight', () => {
+    let button = render(<Button>classting</Button>);
+    expect(button.container.getElementsByClassName('bg-blue-500').length).toBe(1);
+    button = render(<Button intentWeight={200}>classting</Button>);
+    expect(button.container.getElementsByClassName('bg-blue-200').length).toBe(1);
+
+  });
+
+  test('Button: Hover', () => {
+    let button = render(<Button>classting</Button>);
+    fireEvent.mouseOver(button.container);
+    expect(button.container.getElementsByClassName('hover:bg-blue-400').length).toBe(1);
+
+    button = render(<Button intentWeight={200}>classting</Button>);
+    fireEvent.mouseOver(button.container);
+    expect(button.container.getElementsByClassName('hover:bg-blue-100').length).toBe(1);
+
   });
 });
